391dae5af3ccb6ff241dccd3b95ecf384e5ff8e7
Eden <edenstevens101@gmail.com>
2017-03-10 17:53:34 +0000

    Tidy up pet_shop.rb, added comments

--- weekend_homework_start_point/pet_shop.rb
+++ weekend_homework_start_point/pet_shop.rb
@@ -1,24 +1,30 @@
+#==[[Function #1]==
 def pet_shop_name (pet_shop)
   return pet_shop[:name]
 end
 
+#==[[Function #2]==
 def total_cash (total)
   return total[:admin][:total_cash]
 end
 
+#==[[Function #3]]==
 def add_or_remove_cash (shop, cash)
   shop[:admin][:total_cash] += cash
 end
 
+#==[[Function #4]]==
 def pets_sold(shop)
   return shop[:admin][:pets_sold]
 end
 
+#==[[Function #5]]==
 def increase_pets_sold(shop, num)
   shop[:admin][:pets_sold] += num
 end
 
 # ========================================
+#==[[Function #6]]==
 # def stock_count(shop)
 #   stock = []
 #     for pets in shop
@@ -33,6 +39,7 @@ end
 
 # ========================================
 # ========================================
+#==[[Function #7]]==
 # def pets_by_breed(shop, breed)
 #   count = 0
 #     for pet in shop[:pets]
@@ -53,7 +60,7 @@ def pets_by_breed(shop, breed)
   return count
 end
 # ========================================
-
+#==[[Function #8]]==
 def find_pet_by_name(shop, name)
     for pet in shop[:pets]
       if pet[:name] == name
@@ -63,6 +70,7 @@ def find_pet_by_name(shop, name)
   return nil
 end
 
+#==[[Function #9]]==
 def remove_pet_by_name(shop, name)
   for pet in shop[:pets]
     if pet[:name] == name
@@ -71,33 +79,29 @@ def remove_pet_by_name(shop, name)
   end
 end
 
+#==[[Function #10]]==
 def add_pet_to_stock(shop, new_pet)
   shop[:pets] << new_pet
 end
 
+#==[[Function #11]]==
 def customer_pet_count(customer)
   count = 0
       for pet in customer[:pets]
-        #if pet == nil
-          #count << pet
-          #count << nil
-          #cannot do this as for example
-          #0 << nil explodes
-        #end
         count += 1
       end
   return count
-
   #return customer[:pets].length
 end
 
+#==[[Function #12]]==
 def add_pet_to_customer(customer, new_pet)
   customer[:pets] << new_pet
 end
 
 
 # ========== OPTIONAL ==========
-
+#==[[Function #13]]==
 def customer_can_afford_pet(customer, new_pet)
     if customer[:cash] >= new_pet[:price]
       return true
@@ -105,6 +109,8 @@ def customer_can_afford_pet(customer, new_pet)
   return false
 end
 
+
+#==[[Function #14]]==
 def sell_pet_to_customer(shop, pet, customer)
   if pet == nil
     return
